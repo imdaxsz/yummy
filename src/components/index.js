966098e730765d44e1be9e4435b0443a0aaf4866
mihee <s2809h29@gmail.com>
2024-02-16 16:28:31 +0900

    feat: 컴포넌트 구현 #1

---
+++ src/components/index.js
@@ -0,0 +1,63 @@
+export default class Component {
+  $target;
+  $props;
+  $state;
+
+  constructor($target, $props) {
+    this.$target = $target;
+    this.$props = $props;
+    this.$state = {};
+    this.setup();
+    this.setEvent();
+    this.mount();
+  }
+
+  // state 초기화
+  setup() {}
+
+  template() {
+    return '';
+  }
+
+  render() {
+    const template = this.template();
+    if (template) {
+      this.$target.innerHTML = template;
+    }
+  }
+
+  // 컴포넌트가 마운트 되었을 때
+  mount() {
+    this.render();
+    this.didMount();
+  }
+
+  update() {
+    this.render();
+    this.didUpdate();
+  }
+
+  didMount() {}
+  didUpdate() {}
+
+  setState(newState) {
+    const updatedState = { ...this.$state, ...newState };
+    if (JSON.stringify(this.$state) === JSON.stringify(updatedState)) {
+      return;
+    }
+    this.state = updatedState;
+    this.update();
+  }
+
+  setEvent() {}
+
+  addEvent(eventType, selector, callback) {
+    const children = [...this.$target.querySelectorAll(selector)];
+    const isTarget = (target) =>
+      children.includes(target) || target.closest(selector);
+    this.$target.addEventListener(eventType, (event) => {
+      if (!isTarget(event.target)) return false;
+      callback(event);
+    });
+  }
+}
