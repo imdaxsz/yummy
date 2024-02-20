0f5da476c55b618309d0a475b9b2a132973d347d
mihee <s2809h29@gmail.com>
2024-02-20 23:56:28 +0900

    feat: Bottom Navigation 구현 #2

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,18 +4,48 @@ import Component from '.';
 export default class Navigation extends Component {
   template() {
     return `
-      <ul class='w-full [&_li]:float-left '>
-        <li><a href='/' data-key='home'>홈</a></li>
-        <li><a href='/write' data-key='write'>글쓰기</a></li>
-        <li><a href='/archive' data-key='archive'>보관함</a></li>
+      <ul class='w-full flex flex-1 gray-1'>
+        <li>
+          <a href='/' data-key='home' class='active'>
+            <i class="ph-fill ph-house"></i>홈
+          </a>
+        </li>
+        <li>
+          <a href='/write' data-key='write'>
+            <i class="ph ph-pencil-line"></i>글쓰기
+          </a>
+        </li>
+        <li>
+          <a href='/archive' data-key='archive'>
+            <i class="ph ph-archive"></i>보관함
+          </a>
+        </li>
       </ul>
       `;
   }
-  
+
+  didMount() {
+    this.setActiveStyle();
+  }
+
   setEvent() {
     this.addEvent('click', 'a', (e) => {
       e.preventDefault();
-      navigate(e.target.href);
-    })
+      const target = e.target.tagName === 'I' ? e.target.parentNode : e.target;
+      navigate(target.href);
+      this.setActiveStyle();
+    });
+    // 뒤로가기 클릭을 통해 이전 페이지 이동 시 스타일 적용 
+    window.addEventListener('popstate', this.setActiveStyle.bind(this));
+  }
+
+  // 현재 페이지 item에 active 스타일 적용
+  setActiveStyle() {
+    const items = this.$target.querySelectorAll('a');
+    items.forEach((item) =>
+      item.href === window.location.href
+        ? item.classList.add('active')
+        : item.classList.remove('active'),
+    );
   }
 }
