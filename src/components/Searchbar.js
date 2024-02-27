b7f121814ac5fcd1ae5fd996eb1101bbfb3b3655
mihee <s2809h29@gmail.com>
2024-02-27 17:15:34 +0900

    feat: searchbar auto focus 기능 구현을 위한 class 추가, 클릭 이벤트 추가 #3

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,12 +2,22 @@ import Component from '@components';
 
 export default class Searchbar extends Component {
   template() {
+    const { autoFocus, placeholder } = this.props;
+    const focus = autoFocus ? 'auto-focus' : '';
     return `
-      <div class='relative'>
-        <input class='w-full h-40 px-12 py-8 border rounded-xl' placeholder='검색' />
+      <div id='container' class='relative'>
+        <input
+          class='${`w-full h-40 px-12 py-8 border rounded-xl ${focus}`}' 
+          placeholder='${placeholder}' 
+        />
         <i class="ph ph-magnifying-glass text-23 absolute
-        bottom-1/2 translate-y-1/2 right-12 text-zinc-400"></i>
+          bottom-1/2 translate-y-1/2 right-12 text-zinc-400"></i>
       </div>
     `;
+  }
+
+  setEvent() {
+    const { onClick } = this.props;
+    if (onClick) this.addEvent('click', '#container', onClick);
   }
 }
