8600b1ab332a7b40c9f2f23f197b9c0736780ad4
mihee <s2809h29@gmail.com>
2024-03-05 23:12:00 +0900

    feat: 리스트 상세 페이지, 회원탈퇴, 로그아웃 라우터 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,19 @@
 import Archive from '@pages/Archive';
 import Home from '@pages/Home';
 import Write from '@pages/Write';
-import Post from './Post';
+import Post from '@/pages/Post';
+import ListInfo from '@/pages/ListInfo';
+import Leave from '@/pages/Leave';
+import SignOut from '@/pages/SignOut';
 
 const routes = [
   { path: /^\/$/, view: Home },
   { path: /^\/write$/, view: Write },
   { path: /^\/post\/[\w]+$/, view: Post },
+  { path: /^\/list\/[\w]+$/, view: ListInfo },
   { path: /^\/archive\?category=(my|likes)$/, view: Archive },
+  { path: /^\/leave$/, view: Leave },
+  { path: /^\/signout$/, view: SignOut },
 ];
 
 export default routes;
