8a1a8dabbf7bf3517bd69f5f6492d4f4c5cd5524
mihee <s2809h29@gmail.com>
2024-03-05 23:10:57 +0900

    feat: Card 컴포넌트 재사용성 향상을 위해 props 및 요소, 조건부 렌더링 추가 #3

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,48 @@ import Component from '@components';
 import navigate from '@utils/Navigate';
 
 export default class Card extends Component {
+  constructor(
+    $target,
+    {
+      cardType = 'list',
+      likeCount = 0,
+      rating = -1,
+      placeLocation = '',
+      isMine = false,
+      ...rest
+    },
+  ) {
+    super($target, { cardType, likeCount, rating, placeLocation, isMine, ...rest });
+  }
+
   template() {
-    const { id, title, userId, likes, thumnail } = this.props;
+    const {
+      id,
+      title,
+      userId,
+      likeCount,
+      rating,
+      placeLocation,
+      isMine,
+      thumnail,
+      cardType,
+    } = this.props;
+    const isListCard = cardType === 'list';
+    const url = isListCard ? `/list/${id}` : `/post/${id}`;
+    const value = isListCard ? likeCount : rating;
+    const icon = isListCard ? 'ph-heart' : 'ph-star';
+    const iconColor = isListCard ? 'text-secondary' : 'text-primary';
+    const isLiked = !isMine && location.pathname === '/archive';
 
     return `
-      <a href='${`/list/${id}`}'>
+      <a class='block relative' href=${url}>
+      ${
+        isLiked
+          ? `<button aria-label='좋아요' class='absolute top-0 right-0 p-2'>
+              <i class='block ph-fill ph-heart text-28 text-secondary-100'></i>
+            </button>`
+          : ``
+      }
         <div
           class='w-full flex-center aspect-[4/3] bg-primary-30
           rounded-lg overflow-hidden text-14'
@@ -21,13 +58,20 @@ export default class Card extends Component {
             : `<i class='ph ph-fork-knife text-[2.5rem] text-white'></i>`
         }
         </div>
-        <h3 class='mt-4 mb-2 font-medium'>${title}</h3>
-        <div class='flex justify-between items-center text-14'>
-          <span class='text-zinc-400'>${userId}</span>
-          <span class='flex-center text-secondary gap-2'>
-            <i class='ph-fill ph-heart'></i>
-            ${likes}
+        <h3 class='mt-4 mb-2 font-medium truncate tracking-tight'>${title}</h3>
+        <div class='flex justify-between items-center text-14  w-full'>
+          <span class='text-zinc-400 shrink-0'>${isListCard ? userId : placeLocation}</span>
+          <div class='flex items-center justify-end gap-4 w-[70%]'>
+            <span class='${`flex-center shrink-0 ${iconColor} gap-2`}'>
+              <i class='${`block ph-fill ${icon} text-16`}'></i>
+              ${value}
             </span>
+            ${
+              !isListCard && isLiked
+                ? `<p class='text-zinc-400 author truncate'>${userId}</p>`
+                : ``
+            }
+          </div>
         </div>
       </a>
     `;
@@ -36,9 +80,14 @@ export default class Card extends Component {
   setEvent() {
     this.addEvent('click', 'a', (e) => {
       e.preventDefault();
+      if (e.target.nodeName === 'BUTTON') return;
       if (!e.currentTarget.childNodes) return;
       const target = e.currentTarget.childNodes[1];
       navigate(target.href);
+    });
+
+    this.addEvent('click', 'button', (e) => {
+      console.log('like button clicked');
     });
   }
 }
