86b1869ed6523217e8794563611f257fd5f4228e
mihee <s2809h29@gmail.com>
2024-03-10 00:00:54 +0900

    feat: Chip 구현 #3

---
+++ src/components/Chip.js
@@ -0,0 +1,66 @@
+import Component from '@components';
+
+// TYPE = SELECT | FILTER | ACTION | EXPLAIN
+
+export default class Chip extends Component {
+  constructor(
+    $target,
+    {
+      type = 'filter',
+      active = false,
+      label = '',
+      onClick = undefined,
+      ...rest
+    },
+  ) {
+    super($target, { type, active, label, onClick, ...rest });
+  }
+
+  template() {
+    const { text, id, type, label, active } = this.props;
+
+    const getChipStyle = () => {
+      const minWidth = type === 'explain' ? 'min-w-98' : 'min-w-79';
+      const cursor = ['explain', 'action'].includes(type) ? 'cursor-default' : 'cursor-pointer';
+      const weight = active ? 'font-medium' : '';
+      const common = `${minWidth} ${cursor} ${weight} w-fit text-14 text-center py-4 px-10 rounded-full border `;
+      let className = common;
+
+      switch (type) {
+        case 'filter':
+          className += active
+            ? 'border-secondary text-secondary'
+            : 'border-neutral-300';
+          break;
+        case 'explain':
+        case 'filtered':
+          className += 'bg-zinc-100 border-zinc-100 flex-center';
+          break;
+        default:
+          className += active
+            ? 'bg-secondary border-secondary text-white'
+            : 'border-neutral-300';
+      }
+      return className;
+    };
+
+    return `
+      <div id=${id} class='${getChipStyle()}'>
+        ${type === 'explain' ? `<span class='text-secondary font-medium'>${label}&nbsp;</span>` : ``}
+        <span>${text}</span>
+        ${type === 'filtered' ? `<i class='ph ph-x cursor-pointer text-15 pl-2'></i>` : ``}
+      </div>
+    `;
+  }
+
+  setEvent() {
+    const { type, onClick } = this.props;
+    if (type !== 'explain' && onClick) {
+      const el = type === 'filtered' ? 'i' : 'div';
+      this.addEvent('click', el, (e) => {
+        const { id } = e.currentTarget.childNodes[1];
+        onClick(id);
+      });
+    }
+  }
+}
