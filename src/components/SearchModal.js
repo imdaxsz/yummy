35684d589667d1617d064e0c9d80f17ff6a1d2ff
mihee <s2809h29@gmail.com>
2024-03-10 00:06:41 +0900

    feat: 검색 모달 퍼블리싱 #6

---
+++ src/components/SearchModal.js
@@ -0,0 +1,73 @@
+import Component from '@components';
+import RangeSlider from './RangeSlider';
+import Searchbar from './Searchbar';
+import Chip from './Chip';
+import { FOOD_CATEGORY } from '@constants';
+
+
+
+export default class SearchModal extends Component {
+  setup() {
+    this.state = { filter: [] };
+  }
+  template() {
+    return `
+      <div id='search-header'
+          class='bg-white flex items-center max-w-screen-sm
+          w-full h-60 fixed gap-8 top-0 z-31 px-[1rem]'
+      >
+        <div>
+          <button id='back' aria-label='뒤로가기' class='-ml-2 flex-center'>
+            <i class='ph ph-caret-left text-24 block'></i>
+          </button>
+        </div>
+        <div id='searchbar' class='w-full'></div>
+      </div>
+      <div id='filtered'></div>
+      <div class='px-16 pt-60 h-dvh'>
+        <p class='text-14 font-semibold pt-16 pb-8'>카테고리</p>
+        <div id='chips' class='flex gap-7 mb-8 flex-wrap'></div>
+        <p class='text-14 font-semibold pt-16 pb-8'>별점</p>
+        <div id='slider'></div>
+        <div class='h-300'></div>
+      </div>
+    `;
+  }
+
+  onChipClick(id) {
+    const { filter: prev } = this.state;
+    const filter = prev.includes(id)
+      ? prev.filter((i) => i !== id)
+      : [...prev, id];
+    this.setState({ ...this.state, filter });
+  }
+
+  didMount() {
+    const slider = this.$target.querySelector('#slider');
+    const searchbar = this.$target.querySelector('#searchbar');
+    new RangeSlider(slider);
+    new Searchbar(searchbar, {
+      placeholder: '검색어를 입력하세요.',
+      autoFocus: true,
+    });
+
+    const chips = this.$target.querySelector('#chips');
+    const { filter } = this.state;
+
+    FOOD_CATEGORY.forEach((item) => {
+      const el = document.createElement('div');
+      chips.appendChild(el);
+      new Chip(el, {
+        ...item,
+        type: 'filter',
+        onClick: this.onChipClick.bind(this),
+        active: filter.includes(item.id),
+      });
+    });
+  }
+
+  setEvent() {
+    const { onClick } = this.props;
+    if (onClick) this.addEvent('click', '#back', onClick);
+  }
+}
---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const FOOD_CATEGORY = [
+  { text: '한식', id: 'korean' },
+  { text: '양식', id: 'western' },
+  { text: '일식', id: 'japanese' },
+  { text: '중식', id: 'chinese' },
+  { text: '분식', id: 'flour-based' },
+  { text: '치킨', id: 'chicken' },
+  { text: '패스트푸드', id: 'fastfood' },
+  { text: '퓨전', id: 'fusion' },
+  { text: '아시안', id: 'asian' },
+  { text: '뷔페', id: 'buffet' },
+  { text: '주점', id: 'bar' },
+  { text: '디저트', id: 'dessert' },
+];
