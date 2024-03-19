ecea09cd3ebbf1ab2dfbfd3debac9caeee07023f
mihee <s2809h29@gmail.com>
2024-03-19 23:34:27 +0900

    fix: scrollLock 종료 조건문 삭제, 파라미터 추가

--- src/utils/scrollLock.js
+++ src/utils/scrollLock.js
@@ -1,19 +1,17 @@
-const scrollLock = (display) => {
-  if (window.innerWidth - document.body.clientWidth <= 0) return;
-
+const scrollLock = (display, to = null) => {
   // 스크롤이 있는 경우 document 스타일 수정
   if (display === 'block') {
     document.body.style.cssText = `
       position: fixed; 
-      top: -${window.scrollY}px;
+      top: -${to ? 0 : window.scrollY}px;
       overflow-y: scroll;
       width: 100%;
       `;
   }
   if (display === 'none') {
-    const scrollY = document.body.style.top;
+    const scrollY = to ? `-${to}` : document.body.style.top;
     document.body.style.cssText = '';
-    window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
   }
 };
 
