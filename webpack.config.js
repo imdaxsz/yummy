d44f211bb849e0d0f822e4bc17530ec3fb4f873c
mihee <s2809h29@gmail.com>
2024-04-04 21:49:57 +0900

    feat: api alias 추가

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,7 @@ module.exports = {
     extensions: ['.js'],
     alias: {
       '@': path.resolve(__dirname, 'src/'),
+      '@apis': path.resolve(__dirname, 'src/apis'),
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@constants': path.resolve(__dirname, 'src/constants'),
