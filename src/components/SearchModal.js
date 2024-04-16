5825579a01349508642b5639bcb0bf06fc782eed
mihee <s2809h29@gmail.com>
2024-04-17 00:24:01 +0900

    feat: 검색 모달 퍼블리싱 #6

--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -4,11 +4,9 @@ import RangeSlider from './RangeSlider';
 import Searchbar from './Searchbar';
 import Chip from './Chip';
 
-
-
 export default class SearchModal extends Component {
   setup() {
-    this.state = { filter: [] };
+    this.state = { filter: { categories: [], minScore: 0, maxScore: 5 } };
   }
 
   template() {
@@ -24,8 +22,8 @@ export default class SearchModal extends Component {
         </div>
         <div id='searchbar' class='w-full'></div>
       </div>
-      <div id='filtered'></div>
-      <div class='px-16 pt-60 h-dvh'>
+      <div id='filtered' class='px-16 pt-60 flex gap-7 mb-8 flex-wrap'></div>
+      <div class='px-16 h-dvh'>
         <p class='text-14 font-semibold pt-16 pb-8'>카테고리</p>
         <div id='chips' class='flex gap-7 mb-8 flex-wrap'></div>
         <p class='text-14 font-semibold pt-16 pb-8'>별점</p>
@@ -36,16 +34,23 @@ export default class SearchModal extends Component {
   }
 
   didMount() {
+    const {
+      filter: { categories, minScore, maxScore },
+    } = this.state;
+
     const slider = this.$target.querySelector('#slider');
     const searchbar = this.$target.querySelector('#searchbar');
-    new RangeSlider(slider);
+    new RangeSlider(slider, {
+      onRangeChange: this.onRangeChange.bind(this),
+      minVal: minScore,
+      maxVal: maxScore,
+    });
     new Searchbar(searchbar, {
       placeholder: '검색어를 입력하세요.',
       autoFocus: true,
     });
 
     const chips = this.$target.querySelector('#chips');
-    const { filter } = this.state;
 
     FOOD_CATEGORY.forEach((item) => {
       const el = document.createElement('div');
@@ -54,9 +59,37 @@ export default class SearchModal extends Component {
         ...item,
         type: 'filter',
         onClick: this.onChipClick.bind(this),
-        active: filter.includes(item.id),
+        active: categories.includes(item.id),
       });
     });
+
+    const filtered = this.$target.querySelector('#filtered');
+    categories.forEach((key) => {
+      const ct = FOOD_CATEGORY.find((obj) => obj.id === key);
+      const el = document.createElement('div');
+      filtered.appendChild(el);
+      new Chip(el, {
+        id: key,
+        text: ct.text,
+        type: 'filtered',
+        onClick: this.onChipClick.bind(this),
+      });
+    });
+
+    if (!(maxScore === 5 && minScore === 0)) {
+      const getLabel = (score) => {
+        if (score === 0) return '방문 전';
+        return `<i class='ph-fill ph-star mr-2'></i>${score}`;
+      }
+      const el = document.createElement('div');
+      filtered.appendChild(el);
+      new Chip(el, {
+        id: 'score',
+        text: `${getLabel(minScore)} ~ ${getLabel(maxScore)}`,
+        type: 'filtered',
+        onClick: this.onChipClick.bind(this),
+      });
+    }
   }
 
   setEvent() {
@@ -65,10 +98,22 @@ export default class SearchModal extends Component {
   }
 
   onChipClick(id) {
-    const { filter: prev } = this.state;
-    const filter = prev.includes(id)
+    const {
+      filter: { categories: prev },
+    } = this.state;
+    const categories = prev.includes(id)
       ? prev.filter((i) => i !== id)
       : [...prev, id];
-    this.setState({ ...this.state, filter });
+    this.setState({
+      ...this.state,
+      filter: { ...this.state.filter, categories },
+    });
+  }
+
+  onRangeChange(target, value) {
+    this.setState({
+      ...this.state,
+      filter: { ...this.state.filter, [target]: value },
+    });
   }
 }
