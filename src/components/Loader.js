0d2bd893a6626710fde76b27723a959755f00b3b
mihee <s2809h29@gmail.com>
2024-04-17 00:23:19 +0900

    fix: Loader 생성 시 scrollLock 적용 #3

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,13 +1,17 @@
 import Component from '@components';
+import scrollLock from '@utils/scrollLock';
 
 export default class Loader extends Component {
+  $container;
+
   constructor({ color = 'primary', backdrop = false, ...rest }) {
     const container = document.createElement('div');
     container.id = 'loader';
     container.className = 'fixed inset-0 z-30 flex-center';
-    if (backdrop) container.style.backgroundColor = 'rgba(255, 255, 255, 0.4)';
+    if (backdrop) container.style.backgroundColor = 'rgba(255, 255, 255, 0.6)';
     document.body.appendChild(container);
     super(container, { backdrop, color, ...rest });
+    this.$container = container;
   }
 
   template() {
@@ -16,5 +20,14 @@ export default class Loader extends Component {
     return `
       <div class='loader ${loaderColor}'></div>
     `;
+  }
+
+  didMount() {
+    scrollLock('block');
+  }
+
+  unmount() {
+    scrollLock('none');
+    this.$container.remove();
   }
 }
