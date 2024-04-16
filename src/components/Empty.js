e732a0c8813119d638a81ec1030540d9038ff555
mihee <s2809h29@gmail.com>
2024-04-17 00:22:34 +0900

    design: 컴포넌트 스타일 수정

--- src/components/Empty.js
+++ src/components/Empty.js
@@ -6,7 +6,7 @@ export default class Empty extends Component {
     return `
       <div class='flex-center flex-col gap-6'>
         <i class='block ph ph-fork-knife text-[2.5rem] text-primary-30 opacity-30'></i>
-        <p class='text-zinc-300'>${message}</p>
+        <p class='text-zinc-400'>${message}</p>
       </div>
     `;
   }
