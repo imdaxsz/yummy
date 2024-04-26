f87ecba41e4610d214e985b1e118ca4cfd565e48
mihee <s2809h29@gmail.com>
2024-04-26 17:04:58 +0900

    chore: firebase hosting 설정

--- src/apis/search.js
+++ src/apis/search.js
@@ -3,12 +3,14 @@ import algoliaClient from '@libs/algolia';
 const search = async (categories, minScore, maxScore, keyword) => {
   const result = { list: [] };
 
+  // 필터 없이 키워드만 있는 경우 리스트 제목에 키워드를 포함하는 리스트 검색
   if (keyword && categories.length === 0 && minScore === 0 && maxScore === 5) {
     const listIndex = algoliaClient.initIndex('list');
     const res = await listIndex.search(keyword);
     result.list = res.hits;
   }
 
+  // 포스트 검색
   const index = algoliaClient.initIndex('posts');
   const searchQuery = {};
 
