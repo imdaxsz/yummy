1e76ad8f6f54e0ba94504c4d831143dd573bbcd2
mihee <s2809h29@gmail.com>
2024-05-14 00:37:55 +0900

    chore: jest 추가 설정 #17

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleNameMapper: {
+    '@components': '<rootDir>/src/components/index.js',
+    '@components/(.*)': '<rootDir>/src/components/$1',
+    '@stores/(.*)': '<rootDir>/src/stores/$1',
+  },
+};
